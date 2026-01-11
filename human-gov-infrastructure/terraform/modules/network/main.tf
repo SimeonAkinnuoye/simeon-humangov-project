d08cb83b5ece9956694aa19434e42a3ec88d29b9
simeonakinnuoye <timilehin44@gmailcom>
2026-01-11 21:53:02 +0100

    nat gateway update

--- human-gov-infrastructure/terraform/modules/network/main.tf
+++ human-gov-infrastructure/terraform/modules/network/main.tf
@@ -42,3 +42,46 @@ resource "aws_route_table_association" "public_assoc_2" {
   subnet_id      = aws_subnet.public_subnet_2.id
   route_table_id = aws_route_table.public_rt.id
 }
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
+  tags = { Name = "humangov-private-subnet" }
+}
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1b" # Different Zone!
+  tags = { Name = "humangov-private-subnet-2" }
+}
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_subnet.id
+  depends_on    = [aws_internet_gateway.igw]
+  tags = { Name = "humangov-nat" }
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+  tags = { Name = "humangov-private-rt" }
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_rt.id
+}
+resource "aws_route_table_association" "private_assoc_2" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+
--- human-gov-infrastructure/terraform/modules/network/outputs.tf
+++ human-gov-infrastructure/terraform/modules/network/outputs.tf
@@ -2,10 +2,10 @@ output "vpc_id" {
   value = aws_vpc.main_vpc.id
 }
 
-#output "subnet_id" {
-  #value = aws_subnet.public_subnet.id
-#}
-
 output "public_subnets" {
   value = [aws_subnet.public_subnet.id, aws_subnet.public_subnet_2.id]
+}
+
+output "private_subnets" {
+  value = [aws_subnet.private_subnet.id, aws_subnet.private_subnet_2.id]
 }
